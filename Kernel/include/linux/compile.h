cf13c1b848f477c7e87979364e497df996bbb5b0
Travis Tabbal <travis@tabbal.net>
2011-01-14 11:04:03 -0700

    Adding previously ignored include files. Kernel .gitignore claims they are generated?

---
+++ Kernel/include/linux/compile.h
@@ -0,0 +1,8 @@
+/* This file is auto generated, version 34 */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#34 Fri Jan 14 10:46:35 MST 2011"
+#define LINUX_COMPILE_TIME "10:46:35"
+#define LINUX_COMPILE_BY "travis"
+#define LINUX_COMPILE_HOST "travis-laptop"
+#define LINUX_COMPILE_DOMAIN
+#define LINUX_COMPILER "gcc version 4.5.1 (Sourcery G++ Lite 2010.09-51) "
